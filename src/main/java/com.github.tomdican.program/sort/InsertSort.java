9d2e28c8c5323261d64f53ee8524701fd06a5e9e
greatestfutility <futility@yeah.net>
2018-01-30 13:18:15 +0800

    update insert

--- src/main/java/com.github.tomdican.program/sort/InsertSort.java
+++ src/main/java/com.github.tomdican.program/sort/InsertSort.java
@@ -6,17 +6,14 @@ public class InsertSort {
     // insert a element into the right place of the array
     // such as,{1,3,6,8,10,2} ---> {1,2,3,6,8,10}
     public static void insertSort(int[] a){
-        int i = 1;
-        int j = 0;
-        while (i < a.length) {
-            j = i - 1;
+
+         for (int i = 1; i < a.length; i++) {
+            int j = i - 1;
             int temp = a[i];
-            while (j >= 0 && temp < a[j]) {
+            for (;j >= 0 && temp < a[j];j--) {
                 a[j+1] = a[j];
-                j--;
             }
             a[j+1] = temp;
-            i++;
         }
     }
 }
