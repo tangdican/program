9d1cf0c88d367d1131a2c105dfbf8f47c3232336
G2 <g2@G2deMacBook-Air.local>
2018-04-27 18:37:46 +0800

    update

--- src/main/java/com.github.tomdican.program/sort/MergeSort22.java
+++ src/main/java/com.github.tomdican.program/sort/MergeSort22.java
@@ -2,7 +2,12 @@ package com.github.tomdican.program.sort;
 
 import com.github.tomdican.program.Util;
 
-public class MergeSort21 {
+/**
+ * guide and code, https://www.geeksforgeeks.org/merge-sort/
+ * {10,2,6,22,13,23,111,21,227,912,1234}
+ */
+public class MergeSort22 {
+
     public static void sort(int[] input) {
         int len = input.length;
         int partLen = 1;
@@ -18,52 +23,47 @@ public class MergeSort21 {
     public static void merge(int[] input, int partLen) {
         int tempLen = 2*partLen;
         for (int i = 0; i < input.length + tempLen; i += tempLen) {
-            int[] temp = new int[partLen];
-            int j = 0;
-            for (; i+j < input.length && j < tempLen; j++) {
-                temp[j] = input[i+j];
-            }
-
-            sort(temp, j);
-
-            for (int k = 0; i+k < input.length && k < tempLen; k++) {
-                input[i+k] = temp[k];
-            }
+            sort(input, i, tempLen);
         }
     }
 
-    public static void sort(int[] input, int partLen) {
+    public static void sort(int[] input, int startLoc, int partLen) {
         int[] temp = new int[partLen];
-        int mid = partLen/2;
-        int left = 0;
+        int mid = startLoc + partLen/2;
+        int left = startLoc;
         int right = mid;
+        int rightLen = startLoc + partLen;
+        int leftLen = mid;
         int j = 0;
         for (; j < partLen; j++) {
 
-            if (left >= mid || right >= partLen) {
+            if ((right >= input.length) || left >= leftLen || right >= rightLen) {
                 break;
             }
             if (input[left] <= input[right]) {
-                temp[j] = input[left++];
+                temp[j] = input[left];
+                left++;
             } else {
-                temp[j] = input[right++];
+                temp[j] = input[right];
+                right++;
             }
         }
-        while (left < mid) {
+        while (left < input.length && left < leftLen) {
             temp[j++] = input[left++];
         }
-        while (right < partLen) {
+        while (right < input.length && right < rightLen) {
             temp[j++] = input[right++];
         }
         for (int k = 0; k < partLen; k++) {
-            input[k] = temp[k];
+            if (startLoc + k >= input.length) {
+                break;
+            }
+            input[startLoc + k] = temp[k];
         }
     }
 
     public static void main(String[] args) {
         int[] input = {10,123,111,22,13,23,21,21,227,912,2};
-        MergeSort2.sort(input);
+        MergeSort22.sort(input);
     }
 }
-
-
