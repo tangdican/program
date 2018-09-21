b8411a5fc3600c3d1345f9763fd2a0e652ae2657
tomdican <greatestfutility@163.com>
2018-09-21 18:01:03 +0800

    Operations on Min Heap

--- src/main/java/com.github.tomdican.program/tree/Heap.java
+++ src/main/java/com.github.tomdican.program/tree/Heap.java
@@ -1,6 +1,7 @@
 package com.github.tomdican.program.tree;
 
 import com.github.tomdican.program.Util;
+import com.github.tomdican.program.interview.Permutation;
 
 /**
  *
@@ -21,32 +22,182 @@ public class Heap {
     public static void main(String[] args) {
         int arr[] = {2,3,5,1,4,8,10};
         root = new int[arr.length];
+
         createMinHeap(arr);
+        System.out.println("create min heap:");
         Util.printArray(root);
+
+        int min = getMin();
+
+//        decreaseKey(1, len-1);
+//        System.out.println("extract min heap:");
+//        Util.printArray(root, 0, len-1);
+
+//        extractMin();
+//        System.out.println("extract min heap:");
+//        Util.printArray(root, 0, len-1);
+
+
+        delete(4);
+        Util.printArray(root, 0, len-1);
+        
     }
 
+    /**
+     *
+     * input:   2,3,5,1,4,8,10
+     *
+     * output:  1,2,5,3,4,8,10
+     *
+     * @param arr
+     */
     private static void createMinHeap(int[] arr) {
         Node result = null;
         for (int i = 0; i < arr.length; i++) {
-             insertMinHeap(arr[i]);
+            insertMinHeap(arr[i]);
+        }
+    }
+
+
+    /**
+     * input :   1,2,5,3,4,8,10,
+     *
+     * output:   1,2,5,3,10,8,
+     *
+     * @param loc
+     */
+    private static void delete(int loc) {
+        if (loc >= len) {
+            return;
+        }
+
+        decreaseKey(-1, loc);
+        extractMin();
+    }
+
+
+    /**
+     * Decreases value of key at index 'loc' to new_val.
+     *
+     * input:    1,2,5,3,4,8,10,
+     *
+     * output:    1,2,1,3,4,8,5,
+     *
+     * @param newVal
+     * @param loc
+     */
+    private static void decreaseKey(int newVal, int loc) {
+        if (root[loc] > newVal) {
+            root[loc] = newVal;
+            int parentLoc = getParent(loc);
+            int childLoc = loc;
+            while (parentLoc >= 0) {
+                if (root[childLoc] < root[parentLoc]) {
+                    Util.exchange(root, childLoc, parentLoc);
+                    childLoc = parentLoc;
+                    parentLoc = getParent(childLoc);
+                } else {
+                    break;
+                }
+            }
         }
     }
 
     /**
-     * int i = 2p + 1    (i - 1 )/ 2  = p = i/2 - 1/2
-     * int i = 2p + 2    (i - 2 )/ 2 = p = i/2 - 2/2
      *
+     *  Method to remove minimum element (or root) from min heap
+     *
+     * intput:   1,2,5,3,4,8,10,
+     *
+     * output:   2,3,5,10,4,8,
+     *
+     */
+    private static int extractMin() {
+        if (len < 1 ) {
+            return -1;
+        }
+        int min = root[0];
+        root[0] = root[--len];
+
+        minHeap();
+        return min;
+    }
+
+    /**
+     *  move the val of the loc 0  to the right loc
+     *
+     *
+     *  input:   10,2,5,3,4,8,
+     *
+     *  output:  2,3,5,10,4,8,
+     *
+     */
+    private static void minHeap() {
+        int parentLoc = 0;
+        int leftLoc = 2*parentLoc + 1;
+        int rightLoc = 2*parentLoc + 2;
+        while (leftLoc < len || rightLoc < len) {
+
+            int minLoc = leftLoc;
+            if (rightLoc < len && leftLoc < len) {
+                if (root[rightLoc] < root[leftLoc] ) {
+                    minLoc = rightLoc;
+                }
+            } else if ( rightLoc < len ) {
+                minLoc = rightLoc;
+            }
+
+            if (root[minLoc] < root[parentLoc]) {
+                Util.exchange(root, minLoc, parentLoc);
+            }
+
+            parentLoc = minLoc;
+            leftLoc = 2*parentLoc + 1;
+            rightLoc = 2*parentLoc + 2;
+        }
+    }
+
+
+    /**
+     * output: 1
+     *
+     * @return
+     */
+    private static int getMin() {
+
+        return root[0];
+    }
+
+    /**
+     * insert the value into the min heap
+
      * @param val
      */
     private static void insertMinHeap(int val) {
         root[len++] = val;
         int childLoc = len-1;
-        int parentLoc = (childLoc - (2 - (childLoc - (childLoc/2)*2)))/2;
+        int parentLoc = getParent(childLoc);
 
         while (parentLoc >= 0 && root[childLoc] < root[parentLoc]) {
             Util.exchange(root,childLoc,parentLoc);
             childLoc = parentLoc;
-            parentLoc = (childLoc - (2 - (childLoc - (childLoc/2)*2)))/2;
+            parentLoc = getParent(childLoc);
         }
     }
+
+    /**
+     * get parant location of child location
+     *
+     * int i = 2p + 1    (i - 1 )/ 2  = p = i/2 - 1/2
+     * int i = 2p + 2    (i - 2 )/ 2 = p = i/2 - 2/2
+     *
+     *
+     * @param childLoc
+     * @return
+     */
+    private static int getParent(int childLoc) {
+        return (childLoc - (2 - (childLoc - (childLoc/2)*2)))/2;
+    }
+
+
 }
