7dccc75bdf938773a73eb043be16ec9c2f4bce39
tomdican <greatestfutility@163.com>
2018-09-29 17:14:11 +0800

    modify the deadlock

--- src/main/java/com.github.tomdican.program/concurrency/Deadlock.java
+++ src/main/java/com.github.tomdican.program/concurrency/Deadlock.java
@@ -9,16 +9,24 @@ public class Deadlock {
         public String getName() {
             return this.name;
         }
-        public synchronized void bow(Friend bower) {
-            System.out.format("%s: %s"
-                    + "  has bowed to me!%n",
-                this.name, bower.getName());
-            bower.bowBack(this);
+        public void bow(Friend bower) {
+            synchronized (this) {
+                System.out.format(
+                    //   "%s: %s" +
+                    "  has bowed to me!%n"
+                    //   , this.name, bower.getName()
+                );
+                bower.bowBack(bower);
+            }
         }
-        public synchronized void bowBack(Friend bower) {
-            System.out.format("%s: %s"
-                    + " has bowed back to me!%n",
-                this.name, bower.getName());
+        public void bowBack(Friend bower) {
+            synchronized (bower) {
+                System.out.format(
+                    //  "%s: %s" +
+                    " has bowed back to me!%n"
+                    // , this.name, bower.getName()
+                );
+            }
         }
     }
 
