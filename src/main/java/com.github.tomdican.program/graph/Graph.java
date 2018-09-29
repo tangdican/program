e05659dd91ea4b35c63433a33745dd6faede72f1
tomdican <greatestfutility@163.com>
2018-09-29 11:04:36 +0800

    breath first search

--- src/main/java/com.github.tomdican.program/graph/Graph.java
+++ src/main/java/com.github.tomdican.program/graph/Graph.java
@@ -1,8 +1,40 @@
 package com.github.tomdican.program.graph;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Graph {
 
     public static void main(String[] args) {
+        Vertex adj = createGraph();
+        breathFirstSearch(adj,0);
+
+
+    }
+
+    private static void breathFirstSearch(Vertex adj,int start) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        boolean visited[] = new boolean[adj.V];
+        queue.add(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            int s = queue.poll();
+            System.out.print(","+s);
+            LinkedList<Integer> links = adj.adjListArray[s];
+            Iterator<Integer> iterator = links.iterator();
+            while (iterator.hasNext()) {
+                int r = iterator.next();
+                if (!visited[r]) {
+                    queue.add(r);
+                    visited[r] = true;
+                }
+
+            }
+        }
+        System.out.println("");
+    }
+
+    private static Vertex createGraph() {
         int V = 5;
         Vertex adj = new Vertex(V);
         addEdge(adj, 0, 1);
@@ -14,6 +46,7 @@ public class Graph {
         addEdge(adj, 3, 4);
 
         printGraph(adj,V);
+        return adj;
     }
 
     private static void printGraph(Vertex adj, int v) {
