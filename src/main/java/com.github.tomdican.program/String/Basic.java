424e42ef20ec2469ede02fbf8c96307db0ad40b8
tomdican <greatestfutility@163.com>
2018-10-15 18:32:15 +0800

    rearrange the Adjacent Repeat letter

---
+++ src/main/java/com.github.tomdican.program/String/Basic.java
@@ -0,0 +1,54 @@
+package com.github.tomdican.program.String;
+
+/**
+ * Pangram: A pangram or holoalphabetic sentence is a sentence using every letter of a given alphabet at least once.
+ * The best-known English pangram is “The quick brown fox jumps over the lazy dog.”
+
+ Lipogram: A lipogram is a kind of constrained writing or word game consisting of writing paragraphs or longer works in which a particular letter or group of letters is avoided—usually a common vowel, and frequently E, the most common letter in the English language.
+ “The quick brown fox jumped over the lazy dog” omits the letter S
+ *
+ */
+public class Basic {
+
+    public static void main(String[] args) {
+        String str = "abccaccdcaddc";
+        String result = rearrangeAdjacentRepeat(str);
+        System.out.println(result);
+        if (result.length() > 1 && result.charAt(result.length()-1) == result.charAt(result.length()-2)) {
+            System.out.println("not rearrange!!");
+
+        }
+    }
+
+    /**
+     *  input:  abccaccdcaddc
+     *
+     *  output: abcacdcacdcdc
+     *
+     *  source: https://www.geeksforgeeks.org/rearrange-characters-string-no-two-adjacent/
+     *
+     * @param str
+     * @return
+     */
+    private static String rearrangeAdjacentRepeat(String str) {
+        char s[] = str.toCharArray();
+        char prev = s[0];
+        boolean result = true;
+        for (int i = 1; i < s.length; i++) {
+            if (s[i] == prev) {
+                for (int j = i+1; j < s.length; j++) {
+                    if (s[j] != s[i]) {
+                        char temp = s[i];
+                         s[i] = s[j];
+                         s[j] = temp;
+                         j = s.length;
+                    }
+                }
+
+            }
+            prev = s[i];
+        }
+        return String.valueOf(s);
+    }
+
+}
