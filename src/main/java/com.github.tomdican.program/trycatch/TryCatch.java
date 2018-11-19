5f2903e41bc689037edc784a3301d820d9265f36
tomdican <greatestfutility@163.com>
2018-11-19 14:26:51 +0800

    try catch finally

---
+++ src/main/java/com.github.tomdican.program/trycatch/TryCatch.java
@@ -0,0 +1,26 @@
+package com.github.tomdican.program.trycatch;
+
+public class TryCatch {
+
+    public static void main(String[] args) {
+       int x = testFinally();
+       System.out.println(x);
+    }
+
+    private static int testFinally() {
+        int x = 1;
+        try {
+            x = 2;
+            x = 1/0;
+            return x;
+        } catch (Exception e) {
+            e.printStackTrace();
+            x = 3;
+            return x;
+        } finally {
+            x = 4;
+          //  return x;
+        }
+    }
+
+}
