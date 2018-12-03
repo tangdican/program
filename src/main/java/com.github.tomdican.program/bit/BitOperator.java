f53de6323a0edd175480115e5ed3084763b92e67
tomdican <greatestfutility@163.com>
2018-12-03 13:03:33 +0800

    test max int to negative

--- src/main/java/com.github.tomdican.program/bit/BitOperator.java
+++ src/main/java/com.github.tomdican.program/bit/BitOperator.java
@@ -2,43 +2,67 @@ package com.github.tomdican.program.bit;
 
 public class BitOperator {
     public static void main(String [] args){
-        int x31  = 0x80000000;
-        int x311 = 0x7fffffff;
-        int x = 0;
+//        int x31  = 0x80000000;
+//        int x311 = 0x7fffffff;
+//        int x = 0;
+//
+//        println("-1",Integer.toBinaryString(-1));
+//        println("-2",Integer.toBinaryString(-2));
+//
+//        println("0<<1",Integer.toBinaryString(0<<1));
+//        println("-1<<1",Integer.toBinaryString(-1<<1));
+//
+//        println("0>>1",Integer.toBinaryString(0>>1));
+//        println("1>>1",Integer.toBinaryString(1>>1));
+//        println("-1>>1",Integer.toBinaryString(-1>>1));
+//        println("-2>>1",Integer.toBinaryString(-2>>1));
+//
+//
+//        println("2>>>1",Integer.toBinaryString(x= (2>>>1)));
+//        println("1>>>1",Integer.toBinaryString(x= (1>>>1)));
+//        println("0>>>1",Integer.toBinaryString(x= (0>>>1)));
+//        println("-1>>>1",Integer.toBinaryString(x= ((-1)>>>1)));
+//        println("-2>>>1",Integer.toBinaryString(x= ((-2)>>>1)));
+//        println("-3>>>1",Integer.toBinaryString(x= ((-3)>>>1)));
+//        println(Integer.toBinaryString((-1)>>>1)+"",Integer.toString(x));
+//
+//
+//
+//        println("bit>>",Integer.toBinaryString(x31>>1));
+//        println("bit x31",Integer.toString(x31));
+//
+//        println("bit x311",Integer.toString(x311));
+//        println("bit x311",Integer.toBinaryString(x311));
+//        println("max integer",String.valueOf(Math.pow(2,31)));
+//       println("max test",String.valueOf(2147483647));
+//       println("a^b^b is a:",String.valueOf((char)('a'^'b'^'b')));
 
-        println("-1",Integer.toBinaryString(-1));
-        println("-2",Integer.toBinaryString(-2));
+       testMaxIntToNegative();
+    }
 
-        println("0<<1",Integer.toBinaryString(0<<1));
-        println("-1<<1",Integer.toBinaryString(-1<<1));
+    /**
+     * output:
+     *
+     *   2147483647
+         -2147483648
+     *
+     */
+    private static void testMaxIntToNegative() {
+        int i = 0;
+        while (true) {
+            if (i < 0) {
+                System.out.println(i - 1);
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
 
-        println("0>>1",Integer.toBinaryString(0>>1));
-        println("1>>1",Integer.toBinaryString(1>>1));
-        println("-1>>1",Integer.toBinaryString(-1>>1));
-        println("-2>>1",Integer.toBinaryString(-2>>1));
-
-
-        println("2>>>1",Integer.toBinaryString(x= (2>>>1)));
-        println("1>>>1",Integer.toBinaryString(x= (1>>>1)));
-        println("0>>>1",Integer.toBinaryString(x= (0>>>1)));
-        println("-1>>>1",Integer.toBinaryString(x= ((-1)>>>1)));
-        println("-2>>>1",Integer.toBinaryString(x= ((-2)>>>1)));
-        println("-3>>>1",Integer.toBinaryString(x= ((-3)>>>1)));
-        println(Integer.toBinaryString((-1)>>>1)+"",Integer.toString(x));
-
-
-
-        println("bit>>",Integer.toBinaryString(x31>>1));
-        println("bit x31",Integer.toString(x31));
-
-        println("bit x311",Integer.toString(x311));
-        println("bit x311",Integer.toBinaryString(x311));
-        println("max integer",String.valueOf(Math.pow(2,31)));
-       println("max test",String.valueOf(2147483647));
-       println("a^b^b is a:",String.valueOf((char)('a'^'b'^'b')));
     }
 
     public static void println(String name , String str){
         System.out.println("-->"+name +" : "+ str);
     }
+
+
 }
