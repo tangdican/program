46ab0eb16f7a5c2f099ea557abbd7da2c3999482
tomdican <greatestfutility@163.com>
2018-12-27 19:30:03 +0800

    mbean

---
+++ code/src/main/java/com.github.tomdican.program/util/pyrmont/standardmbeantest/CarMBean.java
@@ -0,0 +1,8 @@
+package com.github.tomdican.program.util.pyrmont.standardmbeantest;
+public interface CarMBean {
+
+  public String getColor();
+  public void setColor(String color);
+  public void drive();
+
+}
