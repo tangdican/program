daa4bb9cac358abbb748982fc8065c51e41e4f26
tomdican <greatestfutility@163.com>
2019-02-08 20:14:54 +0800

    test countdownlatch

--- code/src/main/java/com/github/tomdican/program/concurrent/basic/Main.java
+++ code/src/main/java/com/github/tomdican/program/concurrent/basic/Main.java
@@ -2,6 +2,7 @@ package com.github.tomdican.program.concurrent.basic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -16,9 +17,58 @@ public class Main {
 //        testSemaphore();
 //        testSemaphore2();
 
-        testCyclicBarrier();
+//        testCyclicBarrier();
+
+        // CountDownLatch
+        Driver d = new Driver();
+        d.main();
+
     }
 
+
+  static class Driver { // ...
+   void main() throws InterruptedException {
+     int N = 3;
+     CountDownLatch startSignal = new CountDownLatch(1);
+     CountDownLatch doneSignal = new CountDownLatch(N);
+
+     for (int i = 0; i < N; ++i) // create and start threads
+       new Thread(new Worker(startSignal, doneSignal)).start();
+
+     doSomethingElse();            // don't let run yet
+     startSignal.countDown();      // let all threads proceed
+     doSomethingElse();
+     doneSignal.await();// wait for all to finish
+       System.out.println("finished... ");
+   }
+
+   private void doSomethingElse() {
+       System.out.println("do something else!");
+   }
+  }
+
+ static class Worker implements Runnable {
+     private final CountDownLatch startSignal;
+     private final CountDownLatch doneSignal;
+
+     Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+         this.startSignal = startSignal;
+         this.doneSignal = doneSignal;
+     }
+
+     public void run() {
+         try {
+             startSignal.await();
+             doWork();
+             doneSignal.countDown();
+         } catch (InterruptedException ex) {
+         } // return
+     }
+
+     void doWork() {
+         System.out.println("do work..");
+     }
+ }
     private static void testCyclicBarrier() {
 
         float[][] martrix = {
