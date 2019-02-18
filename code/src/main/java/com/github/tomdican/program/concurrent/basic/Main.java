701b46f559a26a994763cc974f30841c01d0e2cb
tomdican <greatestfutility@163.com>
2019-02-18 23:34:10 +0800

    linkingblocking queue

--- code/src/main/java/com/github/tomdican/program/concurrent/basic/Main.java
+++ code/src/main/java/com/github/tomdican/program/concurrent/basic/Main.java
@@ -2,7 +2,7 @@ package com.github.tomdican.program.concurrent.basic;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Exchanger;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -24,8 +24,105 @@ public class Main {
 //        d.main();
 
         // exchanger
-        testExchanger();
+       // testExchanger();
 
+        testLinkedBlockingQueue();
+
+    }
+
+    private static void testLinkedBlockingQueue() {
+
+        // 建立一个装苹果的篮子
+        Basket basket = new Basket();
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        Producer producer = new Producer("生产者001", basket);
+        Producer producer2 = new Producer("生产者002", basket);
+        Consumer consumer = new Consumer("消费者001", basket);
+        service.submit(producer);
+        service.submit(producer2);
+        service.submit(consumer);
+    }
+
+    public static class Basket {
+        volatile int i = 0;
+        // 篮子，能够容纳3个苹果
+        BlockingQueue<String> basket = new LinkedBlockingQueue<String>(3);
+
+        // 生产苹果，放入篮子
+        public void produce() throws InterruptedException {
+            // put方法放入一个苹果，若basket满了，等到basket有位置
+            basket.put("An apple " + i++);
+        }
+
+        // 生产苹果，放入篮子
+        public void produce(String name) throws InterruptedException {
+            // put方法放入一个苹果，若basket满了，等到basket有位置
+            basket.put(name +" An apple " + i);
+            System.out.println(name +" An apple " + i++);
+
+        }
+
+        // 消费苹果，从篮子中取走
+        public String consume() throws InterruptedException {
+            // take方法取出一个苹果，若basket为空，等到basket有苹果为止(获取并移除此队列的头部)
+            String x = basket.take();
+            System.out.println("消费者take apple : "+ i--);
+            return x;
+
+        }
+    }
+
+    // 定义苹果生产者
+    static class Producer implements Runnable {
+        private String instance;
+        private Basket basket;
+
+        public Producer(String instance, Basket basket) {
+            this.instance = instance;
+            this.basket = basket;
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    // 生产苹果
+//                    System.out.println("生产者准备生产苹果：" + instance);
+                    basket.produce(instance);
+//                    System.out.println("!生产者生产苹果完毕：" + instance);
+                    // 休眠300ms
+                    Thread.sleep(300);
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Producer Interrupted");
+            }
+        }
+    }
+
+    // 定义苹果消费者
+    static class Consumer implements Runnable {
+        private String instance;
+        private Basket basket;
+
+        public Consumer(String instance, Basket basket) {
+            this.instance = instance;
+            this.basket = basket;
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    // 消费苹果
+//                    System.out.println("消费者准备消费苹果：" + instance);
+                    System.out.println(basket.consume());
+//                    System.out.println("!消费者消费苹果完毕：" + instance);
+                    // 休眠1000ms
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Consumer Interrupted");
+            }
+        }
     }
 
     private static void testExchanger() {
